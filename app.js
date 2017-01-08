badbf01df90f2ca75fcca92feb0a6ea22c69f639
cammls <camille.malis@coding-academy.fr>
2017-01-08 16:26:29 +0100

    dependencies and gitignore should work

--- app.js
+++ app.js
@@ -2,15 +2,13 @@ var express  = require('express');
 var app      = express();
 // var router = express.Router();
 
-app.use(express.static('./public')); //bug sans ca! tres important
+app.use(express.static('./')); //bug sans ca! tres important
 //start server
-// var port_number = server.listen(process.env.PORT || 3000);
-// app.listen(port_number);
-app.listen(process.env.PORT || 80888)
+app.listen(process.env.PORT || 8088)
 // app.listen(8088, function () {
 //   console.log('Server listening on port 8088')
 // })
 
 app.get('/', function(req, res) {
-       res.sendfile('./index.html'); // load the single view file (angular will handle the page changes on the front-end)
+       res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
    });
