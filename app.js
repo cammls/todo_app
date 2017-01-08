f807ac26a3fab1a1840a0c9f0da3121573263f9d
cammls <camille.malis@coding-academy.fr>
2017-01-08 15:58:31 +0100

    commit

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ var app      = express();
 
 app.use(express.static('./public')); //bug sans ca! tres important
 //start server
-app.listen(8088, function () {
-  console.log('Server listening on port 8088')
-})
+var port_number = server.listen(process.env.PORT || 3000);
+app.listen(port_number);
+// app.listen(8088, function () {
+//   console.log('Server listening on port 8088')
+// })
 
 app.get('/', function(req, res) {
        res.sendfile('./index.html'); // load the single view file (angular will handle the page changes on the front-end)
